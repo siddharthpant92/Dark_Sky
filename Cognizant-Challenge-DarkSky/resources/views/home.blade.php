f8c1a14747f2d0e78b591ad099b6c6530b277211
Siddharth Pant <siddharthpant92@gmail.com>
2018-01-09 12:25:49 -0700

    added bootstrap

--- Cognizant-Challenge-DarkSky/resources/views/home.blade.php
+++ Cognizant-Challenge-DarkSky/resources/views/home.blade.php
@@ -11,39 +11,27 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
         <!-- Styles -->
        <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}" />
     </head>
     <body>
-        <div class="flex-center position-ref full-height intro-body">
-            <div class="content">
-                <div class="intro" style="color: {{$test['color']}}">
-                  Hi  {{$test['name']}}
-                	<div id="app">
-                		<input type="text" v-model="test" @click="testMethod"></input>@{{test}}
-                	</div>
-                </div>
-            </div>
+        <div id="app">
+            <div class="row border-full">
+				<div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+				<div class="col-sm-4" style="background-color:lavenderblush;">.col-sm-4</div>
+				<div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+			</div>
         </div>
     </body>
 </html>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.27/vue.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script>
     new Vue(
     {
     	el: "#app",
-    	data:
-    	{
-    		test: ''
-    	},
-    	methods:
-    	{
-    		testMethod: function()
-    		{
-    			console.log("here");
-    		}
-    	}
     })
 </script>
