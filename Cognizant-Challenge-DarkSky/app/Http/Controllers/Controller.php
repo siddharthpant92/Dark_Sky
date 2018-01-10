c1f0647eff658d40c9b0dfa62d3413c1877d77f5
Siddharth Pant <siddharthpant92@gmail.com>
2018-01-09 17:11:22 -0700

    making call to dark sky

--- Cognizant-Challenge-DarkSky/app/Http/Controllers/Controller.php
+++ Cognizant-Challenge-DarkSky/app/Http/Controllers/Controller.php
@@ -11,14 +11,36 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    private $key;
+
+    public function __construct()
+    {
+        $this->key = env("DARK_SKY_KEY");
+    }
 
     public function index()
     {
-    	$test = [];
-    	$test['name'] = "Sid";
-    	$test['age'] = "25";
-    	$test['color'] = "red";
+    	$latitude = 40.016457;
+    	$longitude = -105.285884;
+    	// $test = [];
+    	// $test['name'] = "Sid";
+    	// $test['age'] = "25";
+    	// $test['color'] = "red";
+
+    	$forecast = $this->getWeather($latitude, $longitude);
+
+    	//Do a switch on the icon and create an object with certain colors codes and styles to be sent to home.blade. then they can be accessed and used for styling throughout the page
+
+
     	return view("home", [ 
-    		"test"=>$test]);
+    		"forecast"=>$forecast]);
+    }
+
+    public function getWeather($latitude, $longitude)
+    {
+    	$api = "https://api.darksky.net/forecast/".$this->key."/$latitude, $longitude";
+
+    	$forecast = json_decode(file_get_contents($api));
+    	return $forecast;
     }
 }
--- Cognizant-Challenge-DarkSky/resources/views/home.blade.php
+++ Cognizant-Challenge-DarkSky/resources/views/home.blade.php
@@ -11,27 +11,74 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <!-- Styles -->
        <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}" />
     </head>
     <body>
-        <div id="app">
-            <div class="row border-full">
-				<div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
-				<div class="col-sm-4" style="background-color:lavenderblush;">.col-sm-4</div>
-				<div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+        <div class="heading">
+            Weather Forecast
+        </div>
+        <div class="subheading">
+        	<span>Think the weather changes that quickly and want to <a href="{{url('home')}}"><u>check again?</u></a></span>
+        </div>
+
+        <div id="app" class="container" style="padding-top: 50px;">
+        	<div style="text-align: center;">
+				<a class="btn btn-round blue-background" @click="minutely = !minutely">By the Minute</a>
+				<a class="btn btn-round blue-background" @click="hourly = !hourly">By the Hour</a>
+				<a class="btn btn-round blue-background" @click="weekly = !weekly">By the Week</a>
 			</div>
+        	<div class="row" style="text-align: center;">
+        		<div class="row border-full">
+        			<div class="col-lg-6">
+        				<div class="card p4">
+        					Overview	
+        				</div>
+        				
+        			</div>
+        			<div class="col-lg-6">
+        				Summary
+        			</div>
+        		</div>
+			</div>
+			<div style="text-align: center;">
+				<div class="row border-full" v-show="minutely" id="by-minute">
+					Minutely
+				</div>
+			</div>
+			
+			<div style="text-align: center;">
+				<div class="row border-full" v-show="hourly" id="by-hour">
+					hourly
+				</div>
+			</div>
+			
+			<div style="text-align: center;">
+				<div class="row border-full" v-show="weekly" id="by-week">
+					Weekly
+				</div>
+			</div>
+			
         </div>
     </body>
 </html>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.27/vue.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+<!-- Latest compiled and minified CSS -->
 <script>
     new Vue(
     {
     	el: "#app",
+    	data:
+    	{
+    		minutely: false,
+    		hourly: false,
+    		weekly: false
+    	}
     })
 </script>
